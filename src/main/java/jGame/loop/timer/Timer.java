9323922e0c55ba4eac0e80173ce6be2f1768992a
Raymond <yuhongweng0501@gmail.com>
2023-07-10 08:00:52 +0800

    add new value "maxUps" in timer

--- src/main/java/jGame/loop/timer/Timer.java
+++ src/main/java/jGame/loop/timer/Timer.java
@@ -8,11 +8,16 @@ public abstract class Timer {
 
     private int updateTime = 0;
     private int ups = 0;
+    private int maxUps;
 
-
-    public Timer(double updateRateSecond) {
+    public Timer(int maxUps, double updateRateSecond){
         this.UPDATE_RATE = updateRateSecond;
         lastUpdate = System.currentTimeMillis();
+        this.maxUps = maxUps;
+    }
+
+    public Timer(double updateRateSecond) {
+        this(-1, updateRateSecond);
     }
 
     public void update() {
@@ -21,9 +26,11 @@ public abstract class Timer {
         accumulatorUps += currentTimeMillis - lastUpdate;
         lastUpdate = currentTimeMillis;
         if (accumulator > UPDATE_RATE * 1000d) {
-            action();
-            updateTime += 1;
-            accumulator -= UPDATE_RATE * 1000d;
+            if(maxUps == -1 || maxUps > ups){
+                action();
+                updateTime += 1;
+                accumulator -= UPDATE_RATE * 1000d;
+            }
         }
         if (accumulatorUps > 1000d) {
             ups = updateTime;
