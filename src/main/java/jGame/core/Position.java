2a08a44e1db97d19421b3a3cd8b797805b786648
Raymond <yuhongweng0501@gmail.com>
2023-07-19 18:10:45 +0800

    Update Position.java
    Co-Authored-By: greedjesse <67365952+greedjesse@users.noreply.github.com>

--- src/main/java/jGame/core/Position.java
+++ src/main/java/jGame/core/Position.java
@@ -9,7 +9,7 @@ public class Position {
     private final double Y;
 
     /**
-     * create the object
+     * create the object.
      *
      * @param x the X of the position
      * @param y the Y of the position
@@ -20,45 +20,65 @@ public class Position {
     }
 
     /**
-     * get the X of the position
+     * Sets the X of the position.
      *
-     * @return X of the position
+     * @param x the X of the position.
+     * @return the new position.
+     */
+    public Position setX(double x) {
+        return new Position(x, this.Y);
+    }
+
+    /**
+     * get the X of the position.
+     *
+     * @return X of the position.
      */
     public double getX() {
         return X;
     }
 
     /**
-     * get the Y of the position
+     * Sets the Y of the position.
      *
-     * @return Y of the position
+     * @param y the Y of the position.
+     * @return the new position.
+     */
+    public Position setY(double y) {
+        return new Position(this.X, y);
+    }
+
+    /**
+     * get the Y of the position.
+     *
+     * @return Y of the position.
      */
     public double getY() {
         return Y;
     }
 
     /**
-     * get the X of the position in int
+     * get the X of the position in int.
      *
-     * @return X of the position in int
+     * @return X of the position in int.
      */
     public int getIntX() {
         return (int) X;
     }
 
     /**
-     * get the Y of the position in int
+     * get the Y of the position in int.
      *
-     * @return Y of the position in int
+     * @return Y of the position in int.
      */
     public int getIntY() {
         return (int) Y;
     }
 
     /**
-     * get the distance between this and another point
-     * @param position the second position
-     * @return the distance between this and the second position
+     * get the distance between this and another point.
+     * @param position the second position.
+     * @return the distance between this and the second position.
      */
     public double distance(Position position) {
         return Math.sqrt(Math.pow(this.getX() - position.getX(), 2) + Math.pow(this.getY() + position.getY(), 2));
@@ -67,7 +87,7 @@ public class Position {
     /**
      * Adds a specified vector to this instance's elements.
      *
-     * @param vec addend
+     * @param vec addend.
      * @return quotient.
      */
     public Position add(Vector vec) {
