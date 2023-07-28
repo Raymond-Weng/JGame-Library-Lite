005e4b89e8b7c963934646822c2239169a3b1fcf
Raymond <yuhongweng0501@gmail.com>
2023-07-28 10:46:03 +0800

    Update RenderImpl.java
    the resize wasnt working

--- src/main/java/jGame/loop/render/RenderImpl.java
+++ src/main/java/jGame/loop/render/RenderImpl.java
@@ -18,26 +18,26 @@ public class RenderImpl extends Render {
 
     @Override
     public void renderGame() {
-        Image image = new BufferedImage(game.getOutput().getSize().getIntWidth(),
-                game.getOutput().getSize().getIntHeight(),
+        Image image = new BufferedImage(game.getCamera().getDisplayArea().getIntWidth(),
+                game.getCamera().getDisplayArea().getIntHeight(),
                 BufferedImage.TYPE_INT_ARGB);
 
         synchronized (this.game.getObjects()) {
             this.game.getObjects().forEach(arrayList ->
                     arrayList.forEach(gameObject -> {
                         Image object = gameObject.render();
-                        if (new Rectangle(game.getCamera().getPosition().getIntX() - (game.getOutput().getSize().getIntWidth()/2),
-                                game.getCamera().getPosition().getIntY() - (game.getOutput().getSize().getIntHeight()/2),
-                                game.getOutput().getSize().getIntWidth(),
-                                game.getOutput().getSize().getIntHeight())
+                        if (new Rectangle(game.getCamera().getPosition().getIntX() - (game.getCamera().getDisplayArea().getIntWidth()/2),
+                                game.getCamera().getPosition().getIntY() - (game.getCamera().getDisplayArea().getIntHeight()/2),
+                                game.getCamera().getDisplayArea().getIntWidth(),
+                                game.getCamera().getDisplayArea().getIntHeight())
                                 .intersects(new Rectangle(
                                         gameObject.getPosition().getIntX() - (object.getWidth(null) / 2),
                                         gameObject.getPosition().getIntY() - (object.getHeight(null) / 2),
                                         object.getWidth(null),
                                         object.getHeight(null)))) {
                             image.getGraphics().drawImage(object,
-                                    gameObject.getPosition().getIntX() - (object.getWidth(null) / 2) - game.getCamera().getPosition().getIntX() + (game.getOutput().getSize().getIntWidth()/2),
-                                    gameObject.getPosition().getIntY() - (object.getHeight(null) / 2) - game.getCamera().getPosition().getIntY() + (game.getOutput().getSize().getIntHeight()/2),
+                                    gameObject.getPosition().getIntX() - (object.getWidth(null) / 2) - game.getCamera().getPosition().getIntX() + (game.getCamera().getDisplayArea().getIntWidth()/2),
+                                    gameObject.getPosition().getIntY() - (object.getHeight(null) / 2) - game.getCamera().getPosition().getIntY() + (game.getCamera().getDisplayArea().getIntHeight()/2),
                                     null);
                             image.getGraphics().dispose();
                         }
@@ -50,8 +50,8 @@ public class RenderImpl extends Render {
         game.getOutput().getGraphics().drawImage(image,
                 0,
                 0,
-                game.getCamera().getDisplayArea().getIntWidth(),
-                game.getCamera().getDisplayArea().getIntHeight(),
+                game.getOutput().getSize().getIntWidth(),
+                game.getOutput().getSize().getIntHeight(),
                 game.BACKGROUND_COLOR,
                 null);
         game.getOutput().show();
