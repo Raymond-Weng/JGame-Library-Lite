0f79591c4c257c25ad0aa8080413d07e5c5e7673
Raymond <yuhongweng0501@gmail.com>
2023-08-28 14:05:30 +0800

    Create Tutorial.java

---
+++ src/main/java/Tutorial.java
@@ -0,0 +1,12 @@
+import jGame.loop.render.Render;
+
+public class Tutorial extends Render {
+    public Tutorial(double updateRate) {
+        super(updateRate);
+    }
+
+    @Override
+    public void renderGame() {
+
+    }
+}
